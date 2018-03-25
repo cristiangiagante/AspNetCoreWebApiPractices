ab6910636be1c01e6adca8fff2130f86a2052699
Cristian Giagante <cristiangiagante@hotmail.com>
2018-03-25 20:07:27 -0300

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductsApi.Models;
+
+namespace ProductsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private IRepository _Repository;
+
+        public ProductsController(IRepository repository)
+        {
+            _Repository = repository;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _Repository.GetList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _Repository.GetProductById(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Models
+{
+    public interface IRepository
+    {
+        List<Product> GetList();
+
+        Product GetProductById(int id);
+    }
+}
